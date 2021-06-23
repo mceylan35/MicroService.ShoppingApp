c7bf26e3a55a696e6e7d1ee97bad310186963f4e
mceylan35 <mehmetceylan2696@gmail.com>
2021-06-23 23:41:00 +0300

    SubscriptionInfo sınıfı eklendi

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/SubscriptionInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBus.Base
+{
+    public class SubscriptionInfo
+    {
+        public Type HandleType { get; set; }
+
+        public SubscriptionInfo(Type handlerType)
+        {
+            HandleType = handlerType ?? throw new ArgumentNullException(nameof(handlerType));
+
+        }
+        public static SubscriptionInfo Typed(Type handlerType)
+        {
+            return new SubscriptionInfo(handlerType);
+        }
+    }
+}
