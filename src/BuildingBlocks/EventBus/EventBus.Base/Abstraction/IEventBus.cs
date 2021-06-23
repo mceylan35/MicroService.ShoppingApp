30cea97141ddf02853efc0fccf9c0b53d65723da
mceylan35 <mehmetceylan2696@gmail.com>
2021-06-23 23:40:13 +0300

    IEventbus interface eklendi

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
@@ -0,0 +1,16 @@
+﻿using EventBus.Base.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IEventBus
+    {
+        void Publish(IntegrationEvent @event);
+        void Subscribe<T,TH>() where T:IntegrationEvent where TH : IIntegrationEventHandler<T>;
+
+        void UnSubscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+
+    }
+}
